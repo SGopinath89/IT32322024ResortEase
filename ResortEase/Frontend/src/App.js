a2b4a8851f332bc7d004de38ccb4f5733c74b746
SafnaZarook <87115907+SafnaZarook@users.noreply.github.com>
2024-06-28 19:28:49 +0530

    Create App.js

---
+++ ResortEase/Frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Component/NavBar.js';
+import NavBar from './Component/NavBar.js';
+import Home from './Component/Home.js';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Home/>
+      </div>
+  );
+}
+
+export default App;
